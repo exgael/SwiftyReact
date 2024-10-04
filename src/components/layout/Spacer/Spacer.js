0aef6faaa32389b02fcce44ff0392cc9fa47b3dc
exgael <benoitbarbier.dev@gmail.com>
2024-10-04 17:24:44 +0200

    fix(Spacer): fit the current data flow + rmv default props + corrected propsType border to bool

--- src/components/layout/Spacer/Spacer.js
+++ src/components/layout/Spacer/Spacer.js
@@ -2,14 +2,9 @@ import React from 'react';
 import {useContext} from "react";
 import {StackContext} from "../StackContext/index.js";
 import {ViewContext} from "../../core/View/ViewContext.js";
-import {View} from "../../core/index.js";
 import PropTypes from "prop-types";
 
-const Spacer = ({
-                           debugBorder,
-                           minSize = '0px',
-                           ...props
-                       }) => {
+const Spacer = ({debugBorder, minSize = '0px'}) => {
     const { direction } = useContext(StackContext);
     const { requestExpansion } = useContext(ViewContext);
 
@@ -18,30 +13,25 @@ const Spacer = ({
         requestExpansion(direction);
     }, [direction, requestExpansion]);
 
+    const viewStyle = {
+        position: 'relative',
+        boxSizing: 'border-box',
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 0,
+        minWidth: direction === 'row' ? minSize : 'auto',
+        minHeight: direction === 'column' ? minSize : 'auto',
+        border: debugBorder ? '1px solid red' : undefined,
+    };
+
     return (
-        <View
-            debugBorder={debugBorder}
-            direction={direction}
-            style={{
-                flexGrow: 1,
-                flexShrink: 1,
-                flexBasis: 0,
-                minWidth: direction === 'row' ? minSize : 'auto',
-                minHeight: direction === 'column' ? minSize : 'auto',
-            }}
-            {...props}
-        />
+        <div style={viewStyle} />
     );
 };
 
 Spacer.propTypes = {
-    debugBorder: PropTypes.string,
-    minSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
-
-Spacer.defaultProps = {
-    debugBorder: null,
-    minSize: '0px',
+    debugBorder: PropTypes.bool,
+    minSize: PropTypes.string
 };
 
 export default Spacer;
