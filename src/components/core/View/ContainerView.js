5ce3a0a16e90dea3b7465ae14aba76c26a403770
exgael <benoitbarbier.dev@gmail.com>
2024-10-04 17:21:35 +0200

    fix(ContainerView): added StackContext + rmv default prop

--- src/components/core/View/ContainerView.js
+++ src/components/core/View/ContainerView.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { ViewContext } from './ViewContext.js';
 import { useViewModel } from './ViewModel.js';
 import { generateStackStyles } from "./styleUtils.js";
-import { stackDefaultProps, stackViewPropTypes } from "./viewPropTypes.js";
+import { stackViewPropTypes } from "./viewPropTypes.js";
+import { StackContext } from "../../layout/StackContext/index.js";
 
 const ContainerView = ({ children, ...props }) => {
     const { requestExpansion: requestParentExpansion } = useContext(ViewContext);
@@ -21,15 +22,16 @@ const ContainerView = ({ children, ...props }) => {
     };
 
     return (
-        <ViewContext.Provider value={{ direction: props?.direction, requestExpansion }}>
-            <div style={containerStyle}>
-                {children}
-            </div>
-        </ViewContext.Provider>
+        <StackContext.Provider value={{ direction: props?.direction }}>
+            <ViewContext.Provider value={{ requestExpansion }}>
+                <div style={containerStyle}>
+                    {children}
+                </div>
+            </ViewContext.Provider>
+        </StackContext.Provider>
     );
 };
 
 ContainerView.propTypes = stackViewPropTypes;
-ContainerView.defaultProps = stackDefaultProps;
 
 export default ContainerView;
