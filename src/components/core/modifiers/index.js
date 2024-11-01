c677ac5367473dca2ce58ee9ad726a57215bec6f
exgael <benoitbarbier.dev@gmail.com>
2024-11-01 08:12:52 +0100

    refactor(core/modifier): added index.js

---
+++ src/components/core/modifiers/index.js
@@ -0,0 +1,8 @@
+export {  coreModifiersPropTypes, generateCoreViewStyles } from './core';
+export {
+    layoutModifiersPropTypes,
+    internalLayoutModifiersPropTypes,
+    convertInternalLayoutPropsToStyles
+} from './layout';
+
+export {  textModifiersPropTypes } from './text';
